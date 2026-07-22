e3a42d69252686d17f5af6eafed5630a4ab7ae2c
SKP-prog <shawn.personal28@outlook.com>
2026-07-22 19:59:34 +0800

    partial done sprite map

--- src/data/levels.js
+++ src/data/levels.js
@@ -4,37 +4,31 @@
 // Window / door / box are placeholders until you add them to metadata.
 // Pig is an entVy spawn, not a tile — see SPAWNS below.
 
-const V = "outerCornerWalls.midMid"; // void / purple fill
-const OW = ["outerCornerWalls.topMid", "outerCornerWalls.botMid", 
-            "outerCornerWalls.midLeft", "outerCornerWalls.midRight"]; // outer wall
-const OC = ["outerEdgeWalls.topRight", "outerEdgeWalls.topLeft", 
-            "outerEdgeWalls.botRight", "outerEdgeWalls.botLeft"]; // Outer corner wall
-const IC = ["outerCornerWalls.topLeft", "outerCornerWalls.topRight", 
-            "outerCornerWalls.botLeft", "outerCornerWalls.botRight"]; // Inner corner;
-const IW = ["innerWalls.topLeft", "innerWalls.topMid", "innerWalls.topRight",
-            "innerWalls.midLeft", "innerWalls.midMid", "innerWalls.midRight",
-            "innerWalls.botLeft", "innerWalls.botMid", "innerWalls.botRight"];
+const v = "1.1.5"; // void / purple fill
+const OW = ["1.1.8", "1.1.2", "1.1.6", "1.1.4"] // Outer walls [Top, Bottom, Left, Right]
+const OWC = ["1.3.1", "1.3.2", "1.3.3", "1.3.4"] // Outer wall Corner [top left, top right, bottom left, bottom right]
+const IW = ["3.1.2", "3.1.8", "3.1.4", "3.1.6"] // inner wall [top, bottom, left, right]
 
 // prettier-ignore
 export const LEVEL_1 = [
   // 0 - 10 cols
-  [V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V],
+  [v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v],
   // 1 —
-  [V,  V,  OC[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OC[1],  V],
+  [v,  v,  OWC[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OW[0],  OWC[1],  v],
   // 2 — 
-  [V,  V,  OW[2],  IW[0],  IW[1],  IW[1],  IW[1],  IW[1],  IW[1],  IW[1],  IW[1],  IW[1],  IW[1],  IW[1],  IW[1],  IW[2],  OW[3],  V],
-  // 3 — 
-  [V,  V,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[5],  OW[3],  V],
-  // 4
-  [V,  V,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[5],  OW[3],  V],
-  // 5 — 
-  [V,  V,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  V,  IW[7],  IW[7],  IW[7],  IW[7],  IW[7],  IW[7],  IW[7],  IW[8],  OW[3],  V],
-  // 6 — 
-  [V,  V,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  IW[5],  IC[3],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OC[3],  V],
-  // 7 —
-  [V,  V,  OW[2],  IW[6],  IW[7],  IW[7],  IW[7],  IW[8],  OW[3],  V,  V,  V,  V,  V,  V,  V,  V,  V],
-  // 8 —
-  [V,  V,  OC[2],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OC[3],  V,  V,  V,  V,  V,  V,  V,  V,  V],
-  // 9 — void
-  [V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V,  V],
+  [v,  v,  OW[2],  v,  IW[0],  IW[0],  IW[0],  IW[0],  IW[0],  IW[0],  IW[0],  IW[0],  IW[0],  IW[0],  IW[0],  v,  OW[3],  v],
+  // // 3 — 
+  // [v,  v,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[5],  OW[3], v],
+  // // 4
+  // [v,  v,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[4],  IW[5],  OW[3],  v],
+  // // 5 — 
+  // [v,  v,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  V,  IW[7],  IW[7],  IW[7],  IW[7],  IW[7],  IW[7],  IW[7],  IW[8],  OW[3],  v],
+  // // 6 — 
+  // [v,  v,  OW[2],  IW[3],  IW[4],  IW[4],  IW[4],  IW[5],  IC[3],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OC[3],  v],
+  // // 7 —
+  // [v,  v,  OW[2],  IW[6],  IW[7],  IW[7],  IW[7],  IW[8],  OW[3],  v,  v,  v,  v,  v,  v,  v,  v,  v],
+  // // 8 —
+  // [v,  v,  OC[2],  OW[1],  OW[1],  OW[1],  OW[1],  OW[1],  OC[3],  v,  v,  v,  v,  v,  v,  v,  v,  v],
+  // // 9 — void
+  // [v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v,  v],
 ];
--- src/data/tiles.js
+++ src/data/tiles.js
@@ -2,52 +2,185 @@
 // https://pixelfrog-assets.itch.io/kings-and-pigs
 export const TILE_DATA = {
   cellSize: [32, 32],
-  outerCornerWalls: {
-    botRight: { x: 32, y: 32 }, // spritesheet topLeft
-    botMid: { x: 64, y: 32 }, // spritesheet TopMid
-    botLeft: { x: 96, y: 32 }, // spritesheet TopRight
-    midRight: { x: 32, y: 64 }, // spritesheet midLeft
-    midMid: { x: 64, y: 64 }, // spritesheet midMid
-    midLeft: { x: 96, y: 64 }, // spritesheet midRight
-    topRight: { x: 32, y: 96 }, // spritesheet botLeft
-    topMid: { x: 64, y: 96 }, // spritesheet botMid
-    topLeft: { x: 96, y: 96 }, // spritesheet botRight
+  1: {
+    1: {
+      desc: "row 1, first set of tiles",
+      1: { x: 32, y: 32 }, // spritesheet topLeft
+      2: { x: 64, y: 32 }, // spritesheet TopMid
+      3: { x: 96, y: 32 }, // spritesheet TopRight
+      4: { x: 32, y: 64 }, // spritesheet midLeft
+      5: { x: 64, y: 64 }, // spritesheet midMid
+      6: { x: 96, y: 64 }, // spritesheet midRight
+      7: { x: 32, y: 96 }, // spritesheet botLeft
+      8: { x: 64, y: 96 }, // spritesheet botMid
+      9: { x: 96, y: 96 }, // spritesheet botRight
+    },
+    2: {
+      desc: "row 1, second set of tiles",
+      1: { x: 160, y: 32 }, // top wall
+      2: { x: 160, y: 64 }, // mid wall
+      3: { x: 160, y: 96 }, // bottom wall
+    },
+    3: {
+      desc: "row 1, third set of tiles",
+      1: { x: 224, y: 32 },
+      2: { x: 256, y: 32 },
+      3: { x: 224, y: 64 },
+      4: { x: 256, y: 64 },
+    },
+    4: {
+      desc: "row 1, forth set of tiles",
+      1: { x: 320, y: 32 },
+      2: { x: 352, y: 32 },
+      3: { x: 320, y: 64 },
+      4: { x: 352, y: 64 },
+    },
+    5: {
+      desc: "row 1, fifth set of tiles",
+      1: { x: 416, y: 32 },
+      2: { x: 448, y: 32 },
+      3: { x: 416, y: 64 },
+      4: { x: 448, y: 64 },
+    },
+    6: {
+      desc: "row 1, last set of tiles",
+      1: { x: 512, y: 32 },
+      2: { x: 512, y: 64 },
+      3: { x: 544, y: 64 },
+    },
   },
-  outerVertProtrudes: {
-    bot: { x: 160, y: 32 }, // spritesheet for platform from outer wall shown on top
-    mid: { x: 160, y: 64 }, // spritesheet for platform from outer wall
-    top: { x: 160, y: 96 }, // spritesheet for platform from outer wall shown below
+  2: {
+    1: {
+      desc: "row 2, first set of tiles",
+      1: { x: 32, y: 160 },
+      2: { x: 64, y: 160 },
+      3: { x: 96, y: 160 },
+    },
+    2: {
+      desc: "row 2, second set of tiles",
+      1: { x: 160, y: 160 },
+    },
+    3: {
+      desc: "row 2, third set of tiles",
+      1: { x: 224, y: 128 },
+      2: { x: 256, y: 128 },
+      3: { x: 224, y: 160 },
+      4: { x: 256, y: 160 },
+    },
+    4: {
+      desc: "row 2, forth set of tiles",
+      1: { x: 320, y: 128 },
+      2: { x: 352, y: 128 },
+      3: { x: 320, y: 160 },
+      4: { x: 352, y: 160 },
+    },
+    5: {
+      desc: "row 2, fifth set of tiles",
+      1: { x: 416, y: 128 },
+      2: { x: 448, y: 128 },
+      3: { x: 416, y: 160 },
+      4: { x: 448, y: 160 },
+    },
+    6: {
+      desc: "row 2, last set of tiles",
+      1: { x: 512, y: 128 },
+      2: { x: 544, y: 128 },
+      3: { x: 512, y: 160 },
+      4: { x: 544, y: 160 },
+    },
   },
-  outerHorizProtrudes: {
-    left: { x: 32, y: 160 },
-    mid: { x: 64, y: 160 },
-    right: { x: 96, y: 160 },
+  3: {
+    1: {
+      desc: "row 3, first set of tiles",
+      1: { x: 32, y: 224 },
+      2: { x: 64, y: 224 },
+      3: { x: 96, y: 224 },
+      4: { x: 32, y: 256 },
+      5: { x: 64, y: 256 },
+      6: { x: 96, y: 256 },
+      7: { x: 32, y: 288 },
+      8: { x: 64, y: 288 },
+      9: { x: 96, y: 288 },
+    },
+    2: {
+      desc: "row 3, second set of tiles.",
+      1: { x: 160, y: 224 },
+      2: { x: 160, y: 256 },
+      3: { x: 160, y: 288 },
+    },
+    3: {
+      desc: "row 3, third set of tiles",
+      1: { x: 224, y: 224 },
+      2: { x: 256, y: 224 },
+      3: { x: 224, y: 256 },
+      4: { x: 256, y: 256 },
+    },
+    4: {
+      desc: "row 3, forth set of tiles",
+      1: { x: 320, y: 224 },
+      2: { x: 352, y: 224 },
+      3: { x: 320, y: 356 },
+      4: { x: 352, y: 256 },
+    },
+    5: {
+      desc: "row 3, fifth set of tiles",
+      1: { x: 416, y: 224 },
+      2: { x: 448, y: 224 },
+      3: { x: 416, y: 256 },
+      4: { x: 448, y: 256 },
+    },
+    6: {
+      desc: "row 3, last set of tiles",
+      1: { x: 512, y: 224 },
+      2: { x: 512, y: 256 },
+      3: { x: 544, y: 356 },
+    },
   },
-  outerAllProtrude: {
-    mid: { x: 160, y: 160 },
+  4: {
+    1: {
+      desc: "last row, first set of tiles",
+      1: { x: 32, y: 352 },
+      2: { x: 64, y: 352 },
+      3: { x: 96, y: 352 },
+    },
+    2: {
+      desc: "last row, second set of tiles",
+      1: { x: 160, y: 352 },
+    },
+    3: {
+      desc: "last row, third set of tiles",
+      1: { x: 224, y: 320 },
+      2: { x: 256, y: 320 },
+      3: { x: 224, y: 352 },
+      4: { x: 256, y: 352 },
+    },
+    4: {
+      desc: "last row, forth set of tiles",
+      1: { x: 320, y: 320 },
+      2: { x: 352, y: 320 },
+      3: { x: 320, y: 352 },
+      4: { x: 352, y: 352 },
+    },
+    5: {
+      desc: "last row, fifth set of tiles",
+      1: { x: 416, y: 320 },
+      2: { x: 448, y: 320 },
+      3: { x: 416, y: 352 },
+      4: { x: 448, y: 352 },
+    },
+    6: {
+      desc: "last row, last set of tiles",
+      1: { x: 512, y: 320 },
+      2: { x: 544, y: 320 },
+      3: { x: 512, y: 352 },
+      4: { x: 544, y: 352 },
+    },
   },
-  outerEdgeWalls: {
-    // For the edge of the walls
-    topRight: { x: 224, y: 32 },
-    topLeft: { x: 256, y: 32 },
-    botRight: { x: 224, y: 64 },
-    botLeft: { x: 256, y: 64 },
-  },
-  innerWalls: {
-    topLeft: { x: 32, y: 224 },
-    topMid: { x: 64, y: 224 },
-    topRight: { x: 96, y: 224 },
-    midLeft: { x: 32, y: 256 },
-    midMid: { x: 64, y: 256 },
-    midRight: { x: 96, y: 256 },
-    botLeft: { x: 32, y: 288 },
-    botMid: { x: 64, y: 288 },
-    botRight: { x: 96, y: 288 },
-  },
+
   decorations: {
-    window: { x: 64, y: 64 }
+    window: { x: 64, y: 64 },
   },
   objects: {
-    box: { x: 64, y: 64 }
-  }
+    box: { x: 64, y: 64 },
+  },
 };
